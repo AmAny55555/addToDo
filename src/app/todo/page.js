f975a1147880c4e71734b36ed1bf999910643d36
AmAny55555 <amanytheismael@gmail.com>
2025-07-18 10:13:42 +0300

    Initial commit - new upload

--- src/app/todo/page.js
+++ src/app/todo/page.js
@@ -261,7 +261,7 @@ export default function TodoPage() {
               <button
                 onClick={() => {
                   Cookies.remove("token");
-                  localStorage.removeItem("profileImage"); // امسح صورة المستخدم القديم
+                  localStorage.removeItem("profileImage");
                   router.push("/login");
                 }}
                 className="w-full text-left px-4 py-2 hover:bg-red-100 text-red-500 flex items-center gap-2"
